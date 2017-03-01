a2d58708f4b1d23c12cea576e60258ad1f5c5458
Chris Pauley <chrispauley92@gmail.com>
2017-03-01 09:47:19 -0600

    Initial Commit

---
+++ document_parser/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  document_parser
+//
+//  Created by Chris Pauley on 3/1/17.
+//  Copyright © 2017 Infegy. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
